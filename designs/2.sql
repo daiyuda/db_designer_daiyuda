eb4a1523f725985e966adf4ec58361fa68a958f9
daiyuda <daiyuda@gmail.com>
2011-12-06 16:45:30 -0500

    Update designs/2.sql

--- designs/2.sql
+++ designs/2.sql
@@ -57,5 +57,5 @@ CREATE TABLE mv_supplycost
 	where
 		p_partkey = ps_partkey
 		and s_suppkey = ps_suppkey
-		and s_nationkey = n_nationkey;
-		and n_regionkey = r_regionkey
+		and s_nationkey = n_nationkey
+		and n_regionkey = r_regionkey;
