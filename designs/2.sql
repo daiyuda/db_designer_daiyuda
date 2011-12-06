b938e0b10a2645388feba11c3fc708a0e2eda076
daiyuda <daiyuda@gmail.com>
2011-12-06 16:43:43 -0500

    create materialized table mv_supplycost

--- designs/2.sql
+++ designs/2.sql
@@ -1,4 +1,5 @@
 -- QUERY 2
+/*
 select
 	s_acctbal,
 	s_name,
@@ -42,4 +43,19 @@ order by
 	n_name,
 	s_name,
 	p_partkey;
+*/
 
+CREATE TABLE mv_supplycost
+	SELECT 
+		ps_supplycost,
+		r_name,
+	FROM 
+		partsupp,
+		supplier,
+		nation,
+		region
+	where
+		p_partkey = ps_partkey
+		and s_suppkey = ps_suppkey
+		and s_nationkey = n_nationkey;
+		and n_regionkey = r_regionkey
