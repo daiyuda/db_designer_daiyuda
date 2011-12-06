2bda0d59ee8033cd19ac4a71002b4fbb018bc9ed
Yuda Dai <daiyuda@gmail.com>
2011-12-06 02:29:39 +0000

    seperated queries

---
+++ queries/15.sql
@@ -0,0 +1,22 @@
+-- QUERY 15
+select
+	s_suppkey,
+	s_name,
+	s_address,
+	s_phone,
+	total_revenue
+from
+	supplier,
+	revenue0
+where
+	s_suppkey = supplier_no
+	and total_revenue = (
+		select
+			max(total_revenue)
+		from
+			revenue0
+	)
+order by
+	s_suppkey;
+
+drop view revenue0;
