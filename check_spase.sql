da29943982bf6c863c14fc4a40e11a1671472ec0
Yuda Dai <daiyuda@gmail.com>
2011-12-06 04:04:44 +0000

    check space consumption

--- check_spase.sql
+++
@@ -1,5 +0,0 @@
-SELECT table_schema "DB name",
-	sum( data_length + index_length ) / 1024 /1024 "Total Size in MB"
-FROM information_schema.TABLES
-WHERE table_schema = "tpch"
-GROUP BY table_schema;
