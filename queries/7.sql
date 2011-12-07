43b31d912469a807b09164e13e0218504c0ea9ca
daiyuda <daiyuda@gmail.com>
2011-12-07 15:23:21 -0500

    Update queries/7.sql

--- queries/7.sql
+++ queries/7.sql
@@ -1,5 +1,18 @@
 -- QUERY 7
 /*
++----+-------------+------------+--------+------------------+---------+---------+---------------------------+---------+---------------------------------+
+| id | select_type | table      | type   | possible_keys    | key     | key_len | ref                       | rows    | Extra                           |
++----+-------------+------------+--------+------------------+---------+---------+---------------------------+---------+---------------------------------+
+|  1 | PRIMARY     | <derived2> | ALL    | NULL             | NULL    | NULL    | NULL                      |    1412 | Using temporary; Using filesort |
+|  2 | DERIVED     | lineitem   | ALL    | PRIMARY,shipdate | NULL    | NULL    | NULL                      | 1500000 | Using where                     |
+|  2 | DERIVED     | supplier   | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.lineitem.L_SUPPKEY   |       1 |                                 |
+|  2 | DERIVED     | n1         | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.supplier.S_NATIONKEY |       1 | Using where                     |
+|  2 | DERIVED     | orders     | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.lineitem.L_ORDERKEY  |       1 |                                 |
+|  2 | DERIVED     | customer   | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.orders.O_CUSTKEY     |       1 |                                 |
+|  2 | DERIVED     | n2         | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.customer.C_NATIONKEY |       1 | Using where                     |
++----+-------------+------------+--------+------------------+---------+---------+---------------------------+---------+---------------------------------+
+*/
+/*
 select
 	supp_nation,
 	cust_nation,
