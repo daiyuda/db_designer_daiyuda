fc8af632deae209d1091fcc9dc515001f8f6a8b2
daiyuda <daiyuda@gmail.com>
2011-12-07 15:55:49 -0500

    Update designs/8.sql

--- designs/8.sql
+++ designs/8.sql
@@ -1,4 +1,5 @@
 -- QUERY 8
+/*
 select
 	o_year,
 	sum(case
@@ -36,3 +37,31 @@ group by
 	o_year
 order by
 	o_year;
+*/
+
+DROP TABLE IF EXISTS mv_8;
+CREATE TABLE mv_8
+	select
+		extract(year from o_orderdate) as o_year,
+		l_extendedprice * (1 - l_discount) as volume,
+		n2.n_name as nation
+	from
+		part,
+		supplier,
+		lineitem,
+		orders,
+		customer,
+		nation n1,
+		nation n2,
+		region,
+		r_name,
+		o_orderdate,
+		p_type
+	where
+		p_partkey = l_partkey
+		and s_suppkey = l_suppkey
+		and l_orderkey = o_orderkey
+		and o_custkey = c_custkey
+		and c_nationkey = n1.n_nationkey
+		and n1.n_regionkey = r_regionkey
+		and s_nationkey = n2.n_nationkey;
