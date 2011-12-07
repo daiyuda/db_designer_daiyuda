d0237514a2e34b27be23cac837e9e1ae0b0807ad
daiyuda <daiyuda@gmail.com>
2011-12-07 15:10:54 -0500

    Update designs/7.sql

--- designs/7.sql
+++ designs/7.sql
@@ -44,21 +44,20 @@ order by
 DROP TABLE IF EXISTS mv_7;
 CREATE TABLE mv_7
 	SELECT 
-		select
-			n1.n_name as supp_nation,
-			n2.n_name as cust_nation,
-			extract(year from l_shipdate) as l_year,
-			l_extendedprice * (1 - l_discount) as volume
-		from
-			supplier,
-			lineitem,
-			orders,
-			customer,
-			nation n1,
-			nation n2
-		where
-			s_suppkey = l_suppkey
-			and o_orderkey = l_orderkey
-			and c_custkey = o_custkey
-			and s_nationkey = n1.n_nationkey
-			and c_nationkey = n2.n_nationkey;
+		n1.n_name as supp_nation,
+		n2.n_name as cust_nation,
+		extract(year from l_shipdate) as l_year,
+		l_extendedprice * (1 - l_discount) as volume
+	from
+		supplier,
+		lineitem,
+		orders,
+		customer,
+		nation n1,
+		nation n2
+	where
+		s_suppkey = l_suppkey
+		and o_orderkey = l_orderkey
+		and c_custkey = o_custkey
+		and s_nationkey = n1.n_nationkey
+		and c_nationkey = n2.n_nationkey;
