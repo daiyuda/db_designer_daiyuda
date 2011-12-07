599ce36c91319c90064dc50f65fa2a69b229278f
daiyuda <daiyuda@gmail.com>
2011-12-07 12:27:46 -0500

    Update queries/5.sql

--- queries/5.sql
+++ queries/5.sql
@@ -1,4 +1,17 @@
 -- QUERY 5
+
+/*
++----+-------------+----------+--------+---------------+---------+---------+---------------------------+---------+----------------------------------------------+
+| id | select_type | table    | type   | possible_keys | key     | key_len | ref                       | rows    | Extra                                        |
++----+-------------+----------+--------+---------------+---------+---------+---------------------------+---------+----------------------------------------------+
+|  1 | SIMPLE      | orders   | ALL    | PRIMARY       | NULL    | NULL    | NULL                      | 1500000 | Using where; Using temporary; Using filesort |
+|  1 | SIMPLE      | customer | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.orders.O_CUSTKEY     |       1 |                                              |
+|  1 | SIMPLE      | nation   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.customer.C_NATIONKEY |       1 |                                              |
+|  1 | SIMPLE      | region   | ALL    | PRIMARY       | NULL    | NULL    | NULL                      |       5 | Using where; Using join buffer               |
+|  1 | SIMPLE      | lineitem | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.orders.O_ORDERKEY    |       1 |                                              |
+|  1 | SIMPLE      | supplier | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.lineitem.L_SUPPKEY   |       1 | Using where                                  |
++----+-------------+----------+--------+---------------+---------+---------+---------------------------+---------+----------------------------------------------+
+*/
 EXPLAIN(
 select
 	n_name,
