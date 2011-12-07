b886d77561dc20a640f81f0bdea16115cf89dadd
daiyuda <daiyuda@gmail.com>
2011-12-07 18:01:19 -0500

    Update queries/11.sql

--- queries/11.sql
+++ queries/11.sql
@@ -30,53 +30,52 @@ order by
 	value desc
 );
 */
-explain(
-select
-	ps_partkey,
-	sum(ps_supplycost * ps_availqty) as value
-from
-	partsupp,
-	supplier,
-	nation
-where
-	ps_suppkey = s_suppkey
-	and s_nationkey = n_nationkey
-	and n_name = 'GERMANY'
-group by
-	ps_partkey having
-		sum(ps_supplycost * ps_availqty) > (
-			select
-				total
-			from
-				mv_11
-			where
-				n_name = 'GERMANY'
-		)
-order by
-	value desc
+EXPLAIN(
+	SELECT
+		ps_partkey,
+		SUM(ps_supplycost * ps_availqty) AS value
+	FROM
+		partsupp,
+		supplier,
+		nation
+	WHERE
+		ps_suppkey = s_suppkey
+		AND s_nationkey = n_nationkey
+		AND n_name = 'GERMANY'
+	GROUP BY
+		ps_partkey HAVING
+			SUM(ps_supplycost * ps_availqty) > (
+				SELECT
+					total
+				FROM
+					mv_11
+				WHERE
+					n_name = 'GERMANY'
+			)
+	ORDER BY
+		value DESC
 );
-/*
-select
+
+SELECT
 	ps_partkey,
-	sum(ps_supplycost * ps_availqty) as value
-from
+	SUM(ps_supplycost * ps_availqty) AS value
+FROM
 	partsupp,
 	supplier,
 	nation
-where
+WHERE
 	ps_suppkey = s_suppkey
-	and s_nationkey = n_nationkey
-	and n_name = 'GERMANY'
-group by
-	ps_partkey having
-		sum(ps_supplycost * ps_availqty) > (
-			select
+	AND s_nationkey = n_nationkey
+	AND n_name = 'GERMANY'
+GROUP BY
+	ps_partkey HAVING
+		SUM(ps_supplycost * ps_availqty) > (
+			SELECT
 				total
-			from
+			FROM
 				mv_11
-			where
+			WHERE
 				n_name = 'GERMANY'
 		)
-order by
-	value desc;
-*/
+ORDER BY
+	value DESC
