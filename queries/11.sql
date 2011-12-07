6b98ede34b3a745bc9a6339310a814d89c6e2496
daiyuda <daiyuda@gmail.com>
2011-12-07 17:40:58 -0500

    Update queries/11.sql

--- queries/11.sql
+++ queries/11.sql
@@ -28,3 +28,27 @@ group by
 order by
 	value desc
 );
+
+select
+	ps_partkey,
+	sum(ps_supplycost * ps_availqty) as value
+from
+	partsupp,
+	supplier,
+	nation
+where
+	ps_suppkey = s_suppkey
+	and s_nationkey = n_nationkey
+	and n_name = 'GERMANY'
+group by
+	ps_partkey having
+		sum(ps_supplycost * ps_availqty) > (
+			select
+				total
+			from
+				mv_11
+			where
+				n_name = 'GERMANY'
+		)
+order by
+	value desc;
