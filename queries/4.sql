fbd4bde10e9988ed284406ec1eb767607c71242d
daiyuda <daiyuda@gmail.com>
2011-12-06 21:43:53 -0500

    Update queries/4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -25,9 +25,9 @@ order by
 
 select
 	o_orderpriority,
-	count(*) as order_count
+	order_count
 from
-	orders
+	mv_order_4
 where
 	o_orderdate >= date '1993-07-01'
 	and o_orderdate < date '1993-07-01' + interval '3' month
@@ -38,8 +38,4 @@ where
 			mv_lineitem_query4
 		where
 			l_orderkey = o_orderkey
-	)
-group by
-	o_orderpriority
-order by
-	o_orderpriority;
+	);
