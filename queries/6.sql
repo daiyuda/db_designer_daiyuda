21c22f388496f11bbb2797aac2a62cffde7d5b64
daiyuda <daiyuda@gmail.com>
2011-12-07 14:46:36 -0500

    Update queries/6.sql

--- queries/6.sql
+++ queries/6.sql
@@ -1,4 +1,5 @@
 -- QUERY 6
+explain(
 select
 	sum(l_extendedprice * l_discount) as revenue
 from
@@ -7,4 +8,5 @@ where
 	l_shipdate >= date '1994-01-01'
 	and l_shipdate < date '1994-01-01' + interval '1' year
 	and l_discount between .06 - 0.01 and .06 + 0.01
-	and l_quantity < 24;
+	and l_quantity < 24
+);
