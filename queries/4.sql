834a81b1188dc890d16c92d6df36d442ab20028e
daiyuda <daiyuda@gmail.com>
2011-12-07 13:50:51 -0500

    Update queries/4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -44,6 +44,20 @@ group by
 order by
 	o_orderpriority;
 */
+explain(
+select
+	o_orderpriority,
+	count(*) as order_count
+from
+	mv_4
+where
+	o_orderdate >= date '1993-07-01'
+	and o_orderdate < date '1993-07-01' + interval '3' month
+group by
+	o_orderpriority
+order by
+	o_orderpriority);
+
 select
 	o_orderpriority,
 	count(*) as order_count
