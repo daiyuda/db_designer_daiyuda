f8bcdc77d28fc65ba69bb1de6e8487059103e5c2
daiyuda <daiyuda@gmail.com>
2011-12-06 20:14:25 -0500

    Update designs/3.sql

--- designs/3.sql
+++ designs/3.sql
@@ -1,4 +1,5 @@
 -- QUERY 3
+/*
 select
 	l_orderkey,
 	sum(l_extendedprice * (1 - l_discount)) as revenue,
@@ -21,3 +22,31 @@ group by
 order by
 	revenue desc,
 	o_orderdate;
+*/
+DROP TABLE IF EXISTS mv_col;
+CREATE TABLE mv_col 
+	SELECT 
+		l_orderkey,
+		sum(l_extendedprice * (1 - l_discount)) as revenue,
+		o_orderdate,
+		o_shippriority,
+		c_mktsegment,
+		o_orderdate,
+		l_shipdate
+	FROM
+		customer,
+		orders,
+		lineitem
+	WHERE
+		c_custkey = o_custkey
+		AND l_orderkey = o_orderkey
+	GROUP BY
+		l_orderkey,
+		o_orderdate,
+		o_shippriority,
+		c_mktsegment,
+		o_orderdate,
+		l_shipdate
+	ORDER BY
+		revenue DESC,
+		o_orderdate;
