24cdb5b6c7f1d1b4eef95f51fe8bd6876018dd86
daiyuda <daiyuda@gmail.com>
2011-12-07 16:58:41 -0500

    Update designs/9.sql

--- designs/9.sql
+++ designs/9.sql
@@ -35,25 +35,25 @@ order by
 */
 DROP TABLE IF EXISTS mv_9;
 CREATE TABLE mv_9
-	select
-		n_name as nation,
-		extract(year from o_orderdate) as o_year,
-		SUM(l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity) as amount,
+	SELECT
+		n_name AS nation,
+		EXTRACT(YEAR FROM o_orderdate) AS o_year,
+		SUM(l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity) AS amount,
 		p_name
-	from
+	FROM
 		part,
 		supplier,
 		lineitem,
 		partsupp,
 		orders,
 		nation
-	where
+	WHERE
 		s_suppkey = l_suppkey
-		and ps_suppkey = l_suppkey
-		and ps_partkey = l_partkey
-		and p_partkey = l_partkey
-		and o_orderkey = l_orderkey
-		and s_nationkey = n_nationkey
+		AND ps_suppkey = l_suppkey
+		AND ps_partkey = l_partkey
+		AND p_partkey = l_partkey
+		AND o_orderkey = l_orderkey
+		AND s_nationkey = n_nationkey
 	GROUP BY
 		nation,
 		o_year,
