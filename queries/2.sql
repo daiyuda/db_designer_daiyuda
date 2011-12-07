1bb70280ffd44b58546bb784e97f44c029cc4c26
daiyuda <daiyuda@gmail.com>
2011-12-07 14:11:36 -0500

    Update queries/2.sql

--- queries/2.sql
+++ queries/2.sql
@@ -1,4 +1,48 @@
 -- QUERY 2
+EXPLAIN(
+select
+	s_acctbal,
+	s_name,
+	n_name,
+	p_partkey,
+	p_mfgr,
+	s_address,
+	s_phone,
+	s_comment
+from
+	part,
+	supplier,
+	partsupp,
+	nation,
+	region
+where
+	p_partkey = ps_partkey
+	and s_suppkey = ps_suppkey
+	and p_size = 15
+	and p_type like '%BRASS'
+	and s_nationkey = n_nationkey
+	and n_regionkey = r_regionkey
+	and r_name = 'EUROPE'
+	and ps_supplycost = (
+		select
+			min(ps_supplycost)
+		from
+			partsupp,
+			supplier,
+			nation,
+			region
+		where
+			p_partkey = ps_partkey
+			and s_suppkey = ps_suppkey
+			and s_nationkey = n_nationkey
+			and n_regionkey = r_regionkey
+			and r_name = 'EUROPE'
+	)
+order by
+	s_acctbal desc,
+	n_name,
+	s_name,
+	p_partkey);
 
 select
 	s_acctbal,
@@ -43,39 +87,3 @@ order by
 	n_name,
 	s_name,
 	p_partkey;
-/*
-SELECT
-	s_acctbal,
-	s_name,
-	n_name,
-	p_partkey,
-	p_mfgr,
-	s_address,
-	s_phone,
-	s_comment
-FROM
-	part,
-	supplier,
-	partsupp,
-	nation,
-	region
-WHERE
-	p_partkey = ps_partkey
-	AND p_size = 15
-	AND p_type like '%BRASS'
-	AND r_name = 'EUROPE'
-	and ps_supplycost = (
-		SELECT 
-			supplycost
-		FROM 
-			mv_supplycost
-		WHERE 
-			p_partkey = ps_partkey
-			AND r_name = 'EUROPE'
-	)
-ORDER BY
-	s_acctbal DESC,
-	n_name,
-	s_name,
-	p_partkey;
-*/
