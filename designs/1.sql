afd4dd283c2bfba743e171c836f6252da8dc34d1
daiyuda <daiyuda@gmail.com>
2011-12-07 14:10:13 -0500

    Update designs/1.sql

--- designs/1.sql
+++ designs/1.sql
@@ -1,23 +1,23 @@
 -- QUERY 1
 DROP TABLE IF EXISTS mv_1;
 CREATE TABLE mv_1
-    SELECT
-        l_returnflag,
-        l_linestatus,
-        l_shipdate,
-        l_discount,
-        l_tax,
-        sum(l_quantity) as sum_qty,
-        sum(l_extendedprice) as sum_base_price,
-        avg(l_quantity) as avg_qty,
-        avg(l_extendedprice) as avg_price,
-        avg(l_discount) as avg_disc,
-        count(*) as count_order
-    from
-        lineitem
-    group by
-        l_returnflag,
-        l_linestatus
-    order by
-        l_returnflag,
-        l_linestatus;
+        SELECT
+                l_returnflag,
+                l_linestatus,
+                l_shipdate,
+                l_discount,
+                l_tax,
+                SUM(l_quantity) AS sum_qty,
+                SUM(l_extendedprice) AS sum_base_price,
+                AVG(l_quantity) AS avg_qty,
+                AVG(l_extendedprice) AS avg_price,
+                AVG(l_discount) AS avg_disc,
+                COUNT(*) AS count_order
+        FROM
+                lineitem
+        GROUP BY
+                l_returnflag,
+                l_linestatus
+        ORDER BY
+                l_returnflag,
+                l_linestatus;
