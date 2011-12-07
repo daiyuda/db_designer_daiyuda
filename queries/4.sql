c509729c531483d60e9ce0d8b377cfcbdc4c559f
daiyuda <daiyuda@gmail.com>
2011-12-06 22:19:47 -0500

    Update queries/4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -1,5 +1,5 @@
 -- QUERY 4
-
+/*
 select
 	o_orderpriority,
 	count(*) as order_count
@@ -21,23 +21,34 @@ group by
 	o_orderpriority
 order by
 	o_orderpriority;
-
-/*
-select
-	o_orderpriority,
-	order_count
-from
-	mv_order_4
-where
-	o_orderdate >= date '1993-07-01'
-	and o_orderdate < date '1993-07-01' + interval '3' month
-	and exists (
-		select
-			*
-		from
-			lineitem
-		where
-			l_orderkey = o_orderkey
-			and l_commitdate < l_receiptdate
-	);
 */
+SELECT
+	o_orderpriority,
+	COUNT(*) AS order_count
+FROM
+	(
+		SELECT 
+			*
+		FROM 
+			orders
+		WHERE
+			o_orderdate >= date '1993-07-01'
+			and o_orderdate < date '1993-07-01' + interval '3' month
+	)
+		AS o,
+	(
+		SELECT 
+			* 
+		FROM
+			lineitem
+		WHERE
+			l_commitdate < l_receiptdate	
+	)
+		AS l
+WHERE
+	o.o_orderkey = l.l_orderkey
+group by
+	o_orderpriority
+order by
+	o_orderpriority;
+
