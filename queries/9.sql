d7712123fe508981c58177d5573213d0a9031c79
daiyuda <daiyuda@gmail.com>
2011-12-07 16:41:20 -0500

    Update queries/9.sql

--- queries/9.sql
+++ queries/9.sql
@@ -1,4 +1,18 @@
 -- QUERY 9
+/*
++----+-------------+------------+--------+---------------+---------+---------+---------------------------+---------+---------------------------------+
+| id | select_type | table      | type   | possible_keys | key     | key_len | ref                       | rows    | Extra                           |
++----+-------------+------------+--------+---------------+---------+---------+---------------------------+---------+---------------------------------+
+|  1 | PRIMARY     | <derived2> | ALL    | NULL          | NULL    | NULL    | NULL                      |   19792 | Using temporary; Using filesort |
+|  2 | DERIVED     | orders     | ALL    | PRIMARY       | NULL    | NULL    | NULL                      | 1500000 |                                 |
+|  2 | DERIVED     | lineitem   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.orders.O_ORDERKEY    |       1 |                                 |
+|  2 | DERIVED     | part       | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.lineitem.L_PARTKEY   |       1 | Using where                     |
+|  2 | DERIVED     | partsupp   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.lineitem.L_PARTKEY   |       1 | Using where                     |
+|  2 | DERIVED     | supplier   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.lineitem.L_SUPPKEY   |       1 |                                 |
+|  2 | DERIVED     | nation     | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.supplier.S_NATIONKEY |       1 |                                 |
++----+-------------+------------+--------+---------------+---------+---------+---------------------------+---------+---------------------------------+
+*/
+/*
 explain(
 select
 	nation,
@@ -32,3 +46,34 @@ group by
 order by
 	nation,
 	o_year desc);
+*/
+explain(
+select
+	nation,
+	o_year,
+	sum(amount) as sum_profit
+from
+	mv_9
+WHERE
+	p_name like '%green%'
+group by
+	nation,
+	o_year
+order by
+	nation,
+	o_year desc);
+
+select
+	nation,
+	o_year,
+	sum(amount) as sum_profit
+from
+	mv_9
+WHERE
+	p_name like '%green%'
+group by
+	nation,
+	o_year
+order by
+	nation,
+	o_year desc
