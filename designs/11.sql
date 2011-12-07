dd0dd80b53b260d36d118b6eda1eaf32aed1a3ee
daiyuda <daiyuda@gmail.com>
2011-12-07 17:38:25 -0500

    Update designs/11.sql

--- designs/11.sql
+++ designs/11.sql
@@ -28,3 +28,18 @@ group by
 order by
 	value desc;
 */
+
+DROP TABLE IF EXISTS mv_11
+	select
+		sum(ps_supplycost * ps_availqty) * 0.0001000000,
+		n_name
+	from
+		partsupp,
+		supplier,
+		nation
+	where
+		ps_suppkey = s_suppkey
+		and s_nationkey = n_nationkey
+		and n_name = 'GERMANY'
+	GROUP BY
+		n_name;
