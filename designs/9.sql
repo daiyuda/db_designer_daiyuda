7366976594e1ad574650ade347697c8aaa34b5b2
daiyuda <daiyuda@gmail.com>
2011-12-07 16:38:05 -0500

    Update designs/9.sql

--- designs/9.sql
+++ designs/9.sql
@@ -1,4 +1,5 @@
 -- QUERY 9
+/*
 select
 	nation,
 	o_year,
@@ -31,3 +32,29 @@ group by
 order by
 	nation,
 	o_year desc;
+*/
+DROP TABLE IF EXISTS mv_9;
+CREATE TABLE mv_9
+	select
+		n_name as nation,
+		extract(year from o_orderdate) as o_year,
+		SUM(l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity) as amount,
+		p_name
+	from
+		part,
+		supplier,
+		lineitem,
+		partsupp,
+		orders,
+		nation
+	where
+		s_suppkey = l_suppkey
+		and ps_suppkey = l_suppkey
+		and ps_partkey = l_partkey
+		and p_partkey = l_partkey
+		and o_orderkey = l_orderkey
+		and s_nationkey = n_nationkey
+	GROUP BY
+		nation,
+		o_year,
+		p_name;
