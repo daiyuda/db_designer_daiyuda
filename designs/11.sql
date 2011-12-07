d878ad5e4b18bd09e984c325d41fa0597ddeac0a
daiyuda <daiyuda@gmail.com>
2011-12-07 17:53:06 -0500

    Update designs/11.sql

--- designs/11.sql
+++ designs/11.sql
@@ -41,6 +41,12 @@ CREATE TABLE mv_11
 	where
 		ps_suppkey = s_suppkey
 		and s_nationkey = n_nationkey
-		and n_name = 'GERMANY'
 	GROUP BY
 		n_name;
+
+DROP INDEX suppkey ON partsupp;
+CREATE INDEX suppkey ON partsupp(ps_suppkey);
+
+DROP INDEX nationkey ON supplier;
+CREATE INDEX nationkey ON supplier(s_nationkey);
+create index nationkey on supplier(s_nationkey);
