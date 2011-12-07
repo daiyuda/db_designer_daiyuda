fa4f7adee28692cd7d38c8ac000fd2e8a1ab0cca
daiyuda <daiyuda@gmail.com>
2011-12-07 17:09:43 -0500

    Update designs/10.sql

--- designs/10.sql
+++ designs/10.sql
@@ -33,5 +33,33 @@ order by
 	revenue desc;
 */
 
-DROP INDEX custkey ON orders;
-CREATE INDEX custkey ON orders (o_custkey);
+DROP TABLE IF EXISTS mv_10;
+CREATE TABLE mv_10
+	select
+		c_custkey,
+		c_name,
+		sum(l_extendedprice * (1 - l_discount)) as revenue,
+		c_acctbal,
+		n_name,
+		c_address,
+		c_phone,
+		c_comment,
+		o_orderdate,
+		l_returnflag
+	from
+		customer,
+		orders,
+		lineitem,
+		nation
+	where
+		c_custkey = o_custkey
+		and l_orderkey = o_orderkey
+		and c_nationkey = n_nationkey
+	group by
+		c_custkey,
+		c_name,
+		c_acctbal,
+		c_phone,
+		n_name,
+		c_address,
+		c_comment;
