44d18d947bdadf19760d666bd8a878adc88cd596
daiyuda <daiyuda@gmail.com>
2011-12-07 14:52:06 -0500

    Update designs/6.sql

--- designs/6.sql
+++ designs/6.sql
@@ -1,4 +1,5 @@
 -- QUERY 6
+/*
 select
 	sum(l_extendedprice * l_discount) as revenue
 from
@@ -8,3 +9,27 @@ where
 	and l_shipdate < date '1994-01-01' + interval '1' year
 	and l_discount between .06 - 0.01 and .06 + 0.01
 	and l_quantity < 24;
+*/
+
+DROP TABLE IF EXISTS mv_6;
+CREATE TABLE mv_6 
+	SELECT 
+		SUM(l_extendedprice * l_discount) AS revenue,
+		l_shipdate,
+		l_discount,
+		l_quantity
+	FROM
+		lineitem
+	GROUP BY
+		l_shipdate,
+		l_discount,
+		l_quantity;
+
+DROP INDEX shipdate ON mv_6;
+CREATE INDEX shipdate ON mv_6 (l_shipdate);
+
+DROP INDEX discount ON mv_6;
+CREATE INDEX discount ON mv_6 (l_discount);
+
+DROP INDEX quantity ON mv_6;
+CREATE INDEX quantity ON mv_6 (l_quantity);
