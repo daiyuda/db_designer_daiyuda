ffcb6dca26bc5356f92dfc6be6db7ef60789ad79
daiyuda <daiyuda@gmail.com>
2011-12-07 14:05:26 -0500

    Update queries/4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -22,51 +22,31 @@ group by
 order by
 	o_orderpriority;
 */
-/*
-select
-	o_orderpriority,
-	count(*) as order_count
-from
-	orders
-where
-	o_orderdate >= date '1993-07-01'
-	and o_orderdate < date '1993-07-01' + interval '3' month
-	and exists (
-		select
-			*
-		from
-			mv_4
-		where
-			l_orderkey = o_orderkey
-	)
-group by
-	o_orderpriority
-order by
-	o_orderpriority;
-*/
-explain(
-select
-	o_orderpriority,
-	sum(order_count) as order_count
-from
-	mv_4
-where
-	o_orderdate >= date '1993-07-01'
-	and o_orderdate < date '1993-07-01' + interval '3' month
-group by
-	o_orderpriority
-order by
-	o_orderpriority);
 
-select
+EXPLAIN(
+	SELECT
+		o_orderpriority,
+		SUM(order_count) AS order_count
+	FROM
+		mv_4
+	WHERE
+		o_orderdate >= date '1993-07-01'
+		AND o_orderdate < date '1993-07-01' + interval '3' month
+	GROUP BY
+		o_orderpriority
+	ORDER BY
+		o_orderpriority
+);
+
+SELECT
 	o_orderpriority,
-	sum(order_count) as order_count
-from
+	SUM(order_count) AS order_count
+FROM
 	mv_4
-where
+WHERE
 	o_orderdate >= date '1993-07-01'
-	and o_orderdate < date '1993-07-01' + interval '3' month
-group by
+	AND o_orderdate < date '1993-07-01' + interval '3' month
+GROUP BY
 	o_orderpriority
-order by
+ORDER BY
 	o_orderpriority;
