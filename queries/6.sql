9731c00b582915f3f85e1598a0ccff8b29257149
daiyuda <daiyuda@gmail.com>
2011-12-07 14:53:28 -0500

    Update queries/6.sql

--- queries/6.sql
+++ queries/6.sql
@@ -1,4 +1,5 @@
 -- QUERY 6
+/*
 explain(
 select
 	sum(l_extendedprice * l_discount) as revenue
@@ -10,3 +11,26 @@ where
 	and l_discount between .06 - 0.01 and .06 + 0.01
 	and l_quantity < 24
 );
+*/
+
+explain(
+select
+	sum(revenue) as revenue
+from
+	mv_6
+where
+	l_shipdate >= date '1994-01-01'
+	and l_shipdate < date '1994-01-01' + interval '1' year
+	and l_discount between .06 - 0.01 and .06 + 0.01
+	and l_quantity < 24
+);
+
+select
+	sum(revenue) as revenue
+from
+	mv_6
+where
+	l_shipdate >= date '1994-01-01'
+	and l_shipdate < date '1994-01-01' + interval '1' year
+	and l_discount between .06 - 0.01 and .06 + 0.01
+	and l_quantity < 24;
