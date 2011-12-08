609ad7ceefcb12ea2cabc9718bf2d4ea3c786972
daiyuda <daiyuda@gmail.com>
2011-12-07 22:59:24 -0500

    Update designs/19.sql

--- designs/19.sql
+++ designs/19.sql
@@ -40,3 +40,43 @@ order by
 	numwait desc,
 	s_name;
 */
+DROP TABLE IF EXISTS mv_19;
+CREATE TABLE mv_19
+	select
+		s_name,
+		count(*) as numwait,
+		o_orderstatus,
+		n_name
+	from
+		supplier,
+		lineitem l1,
+		orders,
+		nation
+	where
+		s_suppkey = l1.l_suppkey
+		and o_orderkey = l1.l_orderkey
+		and l1.l_receiptdate > l1.l_commitdate
+		and exists (
+			select
+				*
+			from
+				lineitem l2
+			where
+				l2.l_orderkey = l1.l_orderkey
+				and l2.l_suppkey <> l1.l_suppkey
+		)
+		and not exists (
+			select
+				*
+			from
+				lineitem l3
+			where
+				l3.l_orderkey = l1.l_orderkey
+				and l3.l_suppkey <> l1.l_suppkey
+				and l3.l_receiptdate > l3.l_commitdate
+		)
+		and s_nationkey = n_nationkey
+	group by
+		s_name,
+		o_orderstatus,
+		n_name;
