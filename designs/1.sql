c0fc4169da22c1c22dd8eeb88b27955505a3809d
daiyuda <daiyuda@gmail.com>
2011-12-08 18:07:53 -0500

    Update designs/1.sql

--- designs/1.sql
+++ designs/1.sql
@@ -1,14 +1,38 @@
 -- QUERY 1
+/*
+select
+        l_returnflag,
+	l_linestatus,
+	sum(l_quantity) as sum_qty,
+	sum(l_extendedprice) as sum_base_price,
+	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
+	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
+	avg(l_quantity) as avg_qty,
+	avg(l_extendedprice) as avg_price,
+	avg(l_discount) as avg_disc,
+	count(*) as count_order
+from
+	lineitem
+where
+	l_shipdate <= date '1998-12-01'
+group by
+	l_returnflag,
+	l_linestatus
+order by
+	l_returnflag,
+	l_linestatus;
+*/
+
 DROP TABLE IF EXISTS mv_1;
 CREATE TABLE mv_1
         SELECT
                 l_returnflag,
                 l_linestatus,
                 l_shipdate,
-                l_discount,
-                l_tax,
                 SUM(l_quantity) AS sum_qty,
                 SUM(l_extendedprice) AS sum_base_price,
+                SUM(l_extendedprice * (1 - l_discount)) as sum_disc_price,
+                SUM(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
                 AVG(l_quantity) AS avg_qty,
                 AVG(l_extendedprice) AS avg_price,
                 AVG(l_discount) AS avg_disc,
@@ -18,5 +42,4 @@ CREATE TABLE mv_1
         GROUP BY
                 l_returnflag,
                 l_linestatus,
-                l_shipdate,
-                l_tax;
+                l_shipdate;
