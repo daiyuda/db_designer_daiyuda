c1192074ae5636a97adf82c653322af192cc2d82
daiyuda <daiyuda@gmail.com>
2011-12-07 21:54:59 -0500

    Update designs/16.sql

--- designs/16.sql
+++ designs/16.sql
@@ -34,4 +34,20 @@ order by
 
 DROP TABLE IF EXISTS mv_16;
 CREATE TABLE mv_16
+	select
+		p_brand,
+		p_type,
+		p_size,
+		count(distinct ps_suppkey) as supplier_cnt,
+		ps_suppkey
+	from
+		partsupp,
+		part
+	where
+		p_partkey = ps_partkey
+	group by
+		p_brand,
+		p_type,
+		p_size,
+		ps_suppkey;
 
