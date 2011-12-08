4aa6654ad4f7457306257aa1161fa1a40dd0b66b
daiyuda <daiyuda@gmail.com>
2011-12-07 21:28:01 -0500

    Update queries/15.sql

--- queries/15.sql
+++ queries/15.sql
@@ -64,6 +64,28 @@ create view revenue0 (supplier_no, total_revenue) as
 	group by
 		l_suppkey;
 
+explain(
+select
+	s_suppkey,
+	s_name,
+	s_address,
+	s_phone,
+	total_revenue
+from
+	supplier,
+	revenue0
+where
+	s_suppkey = supplier_no
+	and total_revenue = (
+		select
+			max(total_revenue)
+		from
+			revenue0
+	)
+order by
+	s_suppkey
+);
+
 select
 	s_suppkey,
 	s_name,
