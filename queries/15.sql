d9f1adca6c292de0ee4aa7d8a61b03fc03d46077
daiyuda <daiyuda@gmail.com>
2011-12-07 19:40:02 -0500

    Update queries/15.sql

--- queries/15.sql
+++ queries/15.sql
@@ -1,4 +1,17 @@
 -- QUERY 15
+
+create view revenue0 (supplier_no, total_revenue) as
+	select
+		l_suppkey,
+		sum(l_extendedprice * (1 - l_discount))
+	from
+		lineitem
+	where
+		l_shipdate >= date '1996-01-01'
+		and l_shipdate < date '1996-01-01' + interval '3' month
+	group by
+		l_suppkey;
+
 select
 	s_suppkey,
 	s_name,
