40ca21e2c6b2a20dfec72b6843124064ceb47bc5
daiyuda <daiyuda@gmail.com>
2011-12-07 20:59:38 -0500

    Update queries/15.sql

--- queries/15.sql
+++ queries/15.sql
@@ -1,4 +1,17 @@
 -- QUERY 15
+
+/*
++----+-------------+------------+--------+---------------+---------+---------+----------------------+---------+----------------------------------------------+
+| id | select_type | table      | type   | possible_keys | key     | key_len | ref                  | rows    | Extra                                        |
++----+-------------+------------+--------+---------------+---------+---------+----------------------+---------+----------------------------------------------+
+|  1 | PRIMARY     | <derived3> | ALL    | NULL          | NULL    | NULL    | NULL                 |    9968 | Using where; Using temporary; Using filesort |
+|  1 | PRIMARY     | supplier   | eq_ref | PRIMARY       | PRIMARY | 4       | revenue0.supplier_no |       1 |                                              |
+|  3 | DERIVED     | lineitem   | ALL    | NULL          | NULL    | NULL    | NULL                 | 1500000 | Using where; Using temporary; Using filesort |
+|  2 | SUBQUERY    | <derived4> | ALL    | NULL          | NULL    | NULL    | NULL                 |    9968 |                                              |
+|  4 | DERIVED     | lineitem   | ALL    | NULL          | NULL    | NULL    | NULL                 | 1500000 | Using where; Using temporary; Using filesort |
++----+-------------+------------+--------+---------------+---------+---------+----------------------+---------+----------------------------------------------+
+*/
+/*
 drop view if exists revenue0;
 
 create view revenue0 (supplier_no, total_revenue) as
@@ -13,7 +26,6 @@ create view revenue0 (supplier_no, total_revenue) as
 	group by
 		l_suppkey;
 
-explain(
 select
 	s_suppkey,
 	s_name,
@@ -32,7 +44,45 @@ where
 			revenue0
 	)
 order by
-	s_suppkey);
+	s_suppkey;
+
+drop view revenue0;
+*/
+drop view if exists revenue0;
+
+create view revenue0 (supplier_no, total_revenue) as
+	select
+		l_suppkey,
+		max(price)
+	from
+		mv_15
+	where
+		l_shipdate >= date '1996-01-01'
+		and l_shipdate < date '1996-01-01' + interval '3' month
+	group by
+		l_suppkey;
+
+EXPLAIN(
+select
+	s_suppkey,
+	s_name,
+	s_address,
+	s_phone,
+	total_revenue
+from
+	supplier,
+	revenue0
+where
+	s_suppkey = supplier_no
+	and total_revenue = (
+		select
+			total_revenue
+		from
+			revenue0
+	)
+order by
+	s_suppkey
+);
 
 select
 	s_suppkey,
@@ -47,7 +97,7 @@ where
 	s_suppkey = supplier_no
 	and total_revenue = (
 		select
-			max(total_revenue)
+			total_revenue
 		from
 			revenue0
 	)
