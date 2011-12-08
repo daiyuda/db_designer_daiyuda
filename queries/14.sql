5f690b11299c22b8fac8cc588b0e34dc9df43f8f
daiyuda <daiyuda@gmail.com>
2011-12-07 20:12:00 -0500

    Update queries/14.sql

--- queries/14.sql
+++ queries/14.sql
@@ -13,7 +13,7 @@ from
 where
 	l_partkey = p_partkey
 	and l_shipdate >= date '1995-09-01'
-	and l_shipdate < date '1995-09-01' + interval '1' month);
+	and l_shipdate < date '1995-09-01' + interval '1' month)
 */
 explain(
 select
