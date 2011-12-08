39bb0df9e423a5f06f976a4f44fe6be8768b7940
daiyuda <daiyuda@gmail.com>
2011-12-07 21:12:11 -0500

    Update designs/15.sql

--- designs/15.sql
+++ designs/15.sql
@@ -42,13 +42,10 @@ DROP TABLE IF EXISTS mv_15;
 CREATE TABLE mv_15
 	SELECT
 		l_suppkey,
-		SUM(l_extendedprice * (1 - l_discount)) AS price,
+		l_extendedprice * (1 - l_discount) AS price,
 		l_shipdate
 	FROM
-		lineitem
-	GROUP BY
-		l_suppkey,
-		l_shipdate;
+		lineitem;
 		
 DROP INDEX shipdate ON mv_15;
 CREATE INDEX shipdate ON mv_15(l_shipdate);
