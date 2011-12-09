311cabe71aa86a6877bc2e7a1ba229f0c803f100
daiyuda <daiyuda@gmail.com>
2011-12-09 16:21:20 -0500

    Update designs/6.sql

--- designs/6.sql
+++ designs/6.sql
@@ -1,15 +1,4 @@
 -- QUERY 6
-/*
-select
-	sum(l_extendedprice * l_discount) as revenue
-from
-	lineitem
-where
-	l_shipdate >= date '1994-01-01'
-	and l_shipdate < date '1994-01-01' + interval '1' year
-	and l_discount between .06 - 0.01 and .06 + 0.01
-	and l_quantity < 24;
-*/
 
 DROP TABLE IF EXISTS mv_6;
 CREATE TABLE mv_6 
