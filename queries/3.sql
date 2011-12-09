65996aefbab88adb6acd179ae1941c34720719cc
daiyuda <daiyuda@gmail.com>
2011-12-08 21:50:12 -0500

    Update queries/3.sql

--- queries/3.sql
+++ queries/3.sql
@@ -9,6 +9,19 @@
 +----+-------------+----------+--------+---------------+---------+---------+------------------------+---------+----------------------------------------------+
 7656 rows in set (38.79 sec)
 */
+
+SELECT
+	l_orderkey,
+	revenue,
+	o_orderdate,
+	o_shippriority
+FROM
+	mv_3
+WHERE
+	c_mktsegment = 'BUILDING'
+	AND o_orderdate < date '1995-03-15'
+	AND l_shipdate > date '1995-03-15';
+
 EXPLAIN(
 	SELECT
 		l_orderkey,
@@ -22,15 +35,3 @@ EXPLAIN(
 		AND o_orderdate < date '1995-03-15'
 		AND l_shipdate > date '1995-03-15'
 );
-
-SELECT
-	l_orderkey,
-	revenue,
-	o_orderdate,
-	o_shippriority
-FROM
-	mv_3
-WHERE
-	c_mktsegment = 'BUILDING'
-	AND o_orderdate < date '1995-03-15'
-	AND l_shipdate > date '1995-03-15';
