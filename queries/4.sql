ddbc1fdcfe9f929f78c1b4b0b8e8224c80efe8b1
daiyuda <daiyuda@gmail.com>
2011-12-08 19:38:32 -0500

    Update queries/4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -1,26 +1,12 @@
 -- QUERY 4
 /*
-select
-	o_orderpriority,
-	count(*) as order_count
-from
-	orders
-where
-	o_orderdate >= date '1993-07-01'
-	and o_orderdate < date '1993-07-01' + interval '3' month
-	and exists (
-		select
-			*
-		from
-			lineitem
-		where
-			l_orderkey = o_orderkey
-			and l_commitdate < l_receiptdate
-	)
-group by
-	o_orderpriority
-order by
-	o_orderpriority;
++----+--------------------+----------+--------+---------------+---------+---------+------------------------+---------+----------------------------------------------+
+| id | select_type        | table    | type   | possible_keys | key     | key_len | ref                    | rows    | Extra                                        |
++----+--------------------+----------+--------+---------------+---------+---------+------------------------+---------+----------------------------------------------+
+|  1 | PRIMARY            | orders   | ALL    | NULL          | NULL    | NULL    | NULL                   | 1500000 | Using where; Using temporary; Using filesort |
+|  2 | DEPENDENT SUBQUERY | lineitem | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.orders.O_ORDERKEY |       1 | Using where                                  |
++----+--------------------+----------+--------+---------------+---------+---------+------------------------+---------+----------------------------------------------+
+5 rows in set (5.06 sec)
 */
 
 EXPLAIN(
