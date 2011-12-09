244312e65710d27852b3ed62a7af51baa5f6fa43
daiyuda <daiyuda@gmail.com>
2011-12-08 19:55:20 -0500

    Update queries/1.sql

--- queries/1.sql
+++ queries/1.sql
@@ -5,8 +5,33 @@
 +----+-------------+----------+------+---------------+------+---------+------+---------+----------------------------------------------+
 |  1 | SIMPLE      | lineitem | ALL  | NULL          | NULL | NULL    | NULL | 1500000 | Using where; Using temporary; Using filesort |
 +----+-------------+----------+------+---------------+------+---------+------+---------+----------------------------------------------+
-4 rows in set (19.78 sec)
+4 rows in set (20.32 sec)
 */
+
+SELECT
+	l_returnflag,
+	l_linestatus,
+	SUM(sum_qty) AS sum_qty,
+	SUM(sum_base_price) AS sum_base_price,
+	SUM(sum_disc_price) AS sum_disc_price,
+	SUM(sum_charge) AS sum_charge,
+	
+	SUM(sum_qty) / SUM(count_order) AS avg_qty,
+	SUM(sum_base_price) / SUM(count_order) AS avg_price,
+	SUM(sum_discount) / SUM(count_order) AS avg_disc,
+
+	SUM(count_order) AS count_order
+FROM
+	mv_1
+WHERE
+	l_shipdate <= date '1998-12-01'
+GROUP BY
+	l_returnflag,
+	l_linestatus
+ORDER BY
+	l_returnflag,
+	l_linestatus;
+
 EXPLAIN(
 	SELECT
 		l_returnflag,
@@ -32,27 +57,3 @@ EXPLAIN(
 		l_returnflag,
 		l_linestatus
 );
-
-SELECT
-	l_returnflag,
-	l_linestatus,
-	SUM(sum_qty) AS sum_qty,
-	SUM(sum_base_price) AS sum_base_price,
-	SUM(sum_disc_price) AS sum_disc_price,
-	SUM(sum_charge) AS sum_charge,
-	
-	SUM(sum_qty) / SUM(count_order) AS avg_qty,
-	SUM(sum_base_price) / SUM(count_order) AS avg_price,
-	SUM(sum_discount) / SUM(count_order) AS avg_disc,
-
-	SUM(count_order) AS count_order
-FROM
-	mv_1
-WHERE
-	l_shipdate <= date '1998-12-01'
-GROUP BY
-	l_returnflag,
-	l_linestatus
-ORDER BY
-	l_returnflag,
-	l_linestatus;
