38029c1a1b252744e15468d212ad542dddce9a4a
daiyuda <daiyuda@gmail.com>
2011-12-09 15:48:47 -0500

    Update queries/4.sql

--- queries/4.sql
+++ queries/4.sql
@@ -23,17 +23,11 @@ GROUP BY
 ORDER BY
 	o_orderpriority;
 
-EXPLAIN(
-	SELECT
-		o_orderpriority,
-		SUM(order_count) AS order_count
-	FROM
-		mv_4
-	WHERE
-		o_orderdate >= date '1993-07-01'
-		AND o_orderdate < date '1993-07-01' + interval '3' month
-	GROUP BY
-		o_orderpriority
-	ORDER BY
-		o_orderpriority
-);
+/*
++----+-------------+-------+------+---------------+------+---------+------+-------+----------------------------------------------+
+| id | select_type | table | type | possible_keys | key  | key_len | ref  | rows  | Extra                                        |
++----+-------------+-------+------+---------------+------+---------+------+-------+----------------------------------------------+
+|  1 | SIMPLE      | mv_4  | ALL  | NULL          | NULL | NULL    | NULL | 12030 | Using where; Using temporary; Using filesort |
++----+-------------+-------+------+---------------+------+---------+------+-------+----------------------------------------------+
+5 rows in set (0.01 sec)
+*/
