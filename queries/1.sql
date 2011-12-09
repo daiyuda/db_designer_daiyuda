bdaed761ca4f30b1f10674b6f6f503cfb27b826f
daiyuda <daiyuda@gmail.com>
2011-12-08 21:33:44 -0500

    Update queries/1.sql

--- queries/1.sql
+++ queries/1.sql
@@ -31,29 +31,11 @@ GROUP BY
 ORDER BY
 	l_returnflag,
 	l_linestatus;
-
-EXPLAIN(
-	SELECT
-		l_returnflag,
-		l_linestatus,
-		SUM(sum_qty) AS sum_qty,
-		SUM(sum_base_price) AS sum_base_price,
-		SUM(sum_disc_price) AS sum_disc_price,
-		SUM(sum_charge) AS sum_charge,
-		
-		SUM(sum_qty) / SUM(count_order) AS avg_qty,
-		SUM(sum_base_price) / SUM(count_order) AS avg_price,
-		SUM(sum_discount) / SUM(count_order) AS avg_disc,
-	
-		SUM(count_order) AS count_order
-	FROM
-		mv_1
-	WHERE
-		l_shipdate <= date '1998-12-01'
-	GROUP BY
-		l_returnflag,
-		l_linestatus
-	ORDER BY
-		l_returnflag,
-		l_linestatus
-);
+/*
++----+-------------+-------+------+---------------+------+---------+------+------+----------------------------------------------+
+| id | select_type | table | type | possible_keys | key  | key_len | ref  | rows | Extra                                        |
++----+-------------+-------+------+---------------+------+---------+------+------+----------------------------------------------+
+|  1 | SIMPLE      | mv_1  | ALL  | NULL          | NULL | NULL    | NULL | 3817 | Using where; Using temporary; Using filesort |
++----+-------------+-------+------+---------------+------+---------+------+------+----------------------------------------------+
+4 rows in set (0.02 sec)
+*/
