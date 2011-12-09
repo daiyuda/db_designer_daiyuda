4d0aa04aef37ab95ec60012024321cb917f283d3
daiyuda <daiyuda@gmail.com>
2011-12-09 16:24:50 -0500

    Update designs/14.sql

--- designs/14.sql
+++ designs/14.sql
@@ -1,23 +1,4 @@
 -- QUERY 14
-/*
-select
-	100.00 * sum(case
-		when p_type like 'PROMO%'
-			then l_extendedprice * (1 - l_discount)
-		else 0
-	end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue
-from
-	lineitem,
-	part
-where
-	l_partkey = p_partkey
-	and l_shipdate >= date '1995-09-01'
-	and l_shipdate < date '1995-09-01' + interval '1' month;
-*/
-/*
-drop index shipdate on lineitem;
-create index shipdate on lineitem(l_shipdate);
-*/
 
 DROP TABLE IF EXISTS mv_14;
 CREATE TABLE mv_14
