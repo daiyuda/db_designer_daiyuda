5692c78dede531c6fdf224fbc50d5c40d638c4d9
daiyuda <daiyuda@gmail.com>
2011-12-08 19:49:14 -0500

    Update queries/7.sql

--- queries/7.sql
+++ queries/7.sql
@@ -11,69 +11,31 @@
 |  2 | DERIVED     | customer   | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.orders.O_CUSTKEY     |       1 |                                 |
 |  2 | DERIVED     | n2         | eq_ref | PRIMARY          | PRIMARY | 4       | tpch.customer.C_NATIONKEY |       1 | Using where                     |
 +----+-------------+------------+--------+------------------+---------+---------+---------------------------+---------+---------------------------------+
+
 */
-/*
-select
-	supp_nation,
-	cust_nation,
-	l_year,
-	sum(volume) as revenue
-from
-	(
-		select
-			n1.n_name as supp_nation,
-			n2.n_name as cust_nation,
-			extract(year from l_shipdate) as l_year,
-			l_extendedprice * (1 - l_discount) as volume
-		from
-			supplier,
-			lineitem,
-			orders,
-			customer,
-			nation n1,
-			nation n2
-		where
-			s_suppkey = l_suppkey
-			and o_orderkey = l_orderkey
-			and c_custkey = o_custkey
-			and s_nationkey = n1.n_nationkey
-			and c_nationkey = n2.n_nationkey
-			and (
-				(n1.n_name = 'FRANCE' and n2.n_name = 'GERMANY')
-				or (n1.n_name = 'GERMANY' and n2.n_name = 'FRANCE')
-			)
-			and l_shipdate between date '1995-01-01' and date '1996-12-31'
-	) as shipping
-group by
-	supp_nation,
-	cust_nation,
-	l_year
-order by
-	supp_nation,
-	cust_nation,
-	l_year;
-*/
+
 EXPLAIN(
-SELECT
-	supp_nation,
-	cust_nation,
-	l_year,
-	SUM(volume) AS revenue
-FROM 
-	mv_7
-WHERE
-	((supp_nation = 'FRANCE' and cust_nation = 'GERMANY')
-		OR (supp_nation = 'GERMANY' and cust_nation = 'FRANCE')	
-	)
-	AND l_shipdate between date '1995-01-01' and date '1996-12-31'
-GROUP BY
-	supp_nation,
-	cust_nation,
-	l_year
-ORDER BY
-	supp_nation,
-	cust_nation,
-	l_year);
+	SELECT
+		supp_nation,
+		cust_nation,
+		l_year,
+		SUM(volume) AS revenue
+	FROM 
+		mv_7
+	WHERE
+		((supp_nation = 'FRANCE' and cust_nation = 'GERMANY')
+			OR (supp_nation = 'GERMANY' and cust_nation = 'FRANCE')	
+		)
+		AND l_shipdate between date '1995-01-01' and date '1996-12-31'
+	GROUP BY
+		supp_nation,
+		cust_nation,
+		l_year
+	ORDER BY
+		supp_nation,
+		cust_nation,
+		l_year
+);
 
 SELECT
 	supp_nation,
