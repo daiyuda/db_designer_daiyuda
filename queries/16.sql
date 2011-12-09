29b00c92ad96156f80f82b2222f17cea616db13d
daiyuda <daiyuda@gmail.com>
2011-12-08 20:53:10 -0500

    Update queries/16.sql

--- queries/16.sql
+++ queries/16.sql
@@ -7,75 +7,38 @@
 |  1 | PRIMARY            | partsupp | eq_ref          | PRIMARY       | PRIMARY | 4       | tpch.part.P_PARTKEY |      1 | Using where                                  |
 |  2 | DEPENDENT SUBQUERY | supplier | unique_subquery | PRIMARY       | PRIMARY | 4       | func                |      1 | Using where                                  |
 +----+--------------------+----------+-----------------+---------------+---------+---------+---------------------+--------+----------------------------------------------+
+18312 rows in set (3.02 sec)
+*/
 
-*/
-/*
-explain(
-select
+SELECT
 	p_brand,
 	p_type,
 	p_size,
-	count(distinct ps_suppkey) as supplier_cnt
-from
-	partsupp,
-	part
-where
-	p_partkey = ps_partkey
-	and p_brand <> 'Brand#45'
-	and p_type not like 'MEDIUM POLISHED%'
-	and p_size in (49, 14, 23, 45, 19, 3, 36, 9)
-	and ps_suppkey not in (
-		select
+	SUM(distinct supplier_cnt) AS supplier_cnt
+FROM
+	mv_16
+WHERE
+	p_brand <> 'Brand#45'
+	AND p_type not like 'MEDIUM POLISHED%'
+	AND p_size in (49, 14, 23, 45, 19, 3, 36, 9)
+	AND ps_suppkey not in (
+		SELECT
 			s_suppkey
-		from
+		FROM
 			supplier
-		where
+		WHERE
 			s_comment like '%Customer%Complaints%'
 	)
-group by
+GROUP BY
 	p_brand,
 	p_type,
 	p_size
-order by
-	supplier_cnt desc,
-	p_brand,
-	p_type,
-	p_size
-);
-*/
-/*
-select
-	p_brand,
-	p_type,
-	p_size,
-	count(distinct ps_suppkey) as supplier_cnt
-from
-	partsupp,
-	part
-where
-	p_partkey = ps_partkey
-	and p_brand <> 'Brand#45'
-	and p_type not like 'MEDIUM POLISHED%'
-	and p_size in (49, 14, 23, 45, 19, 3, 36, 9)
-	and ps_suppkey not in (
-		select
-			s_suppkey
-		from
-			supplier
-		where
-			s_comment like '%Customer%Complaints%'
-	)
-group by
-	p_brand,
-	p_type,
-	p_size
-order by
-	supplier_cnt desc,
+ORDER BY
+	supplier_cnt DESC,
 	p_brand,
 	p_type,
 	p_size;
-*/
-/*
+
 EXPLAIN(
 	SELECT
 		p_brand,
@@ -106,32 +69,4 @@ EXPLAIN(
 		p_type,
 		p_size
 );
-*/
-SELECT
-	p_brand,
-	p_type,
-	p_size,
-	SUM(distinct supplier_cnt) AS supplier_cnt
-FROM
-	mv_16
-WHERE
-	p_brand <> 'Brand#45'
-	AND p_type not like 'MEDIUM POLISHED%'
-	AND p_size in (49, 14, 23, 45, 19, 3, 36, 9)
-	AND ps_suppkey not in (
-		SELECT
-			s_suppkey
-		FROM
-			supplier
-		WHERE
-			s_comment like '%Customer%Complaints%'
-	)
-GROUP BY
-	p_brand,
-	p_type,
-	p_size
-ORDER BY
-	supplier_cnt DESC,
-	p_brand,
-	p_type,
-	p_size;
+
