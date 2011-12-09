753728f30c8a0994eca2ae46aa36bca0ab7346c4
daiyuda <daiyuda@gmail.com>
2011-12-08 20:51:44 -0500

    Update queries/11.sql

--- queries/11.sql
+++ queries/11.sql
@@ -1,35 +1,42 @@
 -- QUERY 11
 /*
-explain(
-select
++----+-------------+----------+--------+---------------+---------+---------+---------------------------+--------+---------------------------------+
+| id | select_type | table    | type   | possible_keys | key     | key_len | ref                       | rows   | Extra                           |
++----+-------------+----------+--------+---------------+---------+---------+---------------------------+--------+---------------------------------+
+|  1 | PRIMARY     | partsupp | ALL    | NULL          | NULL    | NULL    | NULL                      | 200000 | Using temporary; Using filesort |
+|  1 | PRIMARY     | supplier | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.partsupp.PS_SUPPKEY  |      1 |                                 |
+|  1 | PRIMARY     | nation   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.supplier.S_NATIONKEY |      1 | Using where                     |
+|  2 | SUBQUERY    | partsupp | ALL    | NULL          | NULL    | NULL    | NULL                      | 200000 |                                 |
+|  2 | SUBQUERY    | supplier | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.partsupp.PS_SUPPKEY  |      1 |                                 |
+|  2 | SUBQUERY    | nation   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.supplier.S_NATIONKEY |      1 | Using where                     |
++----+-------------+----------+--------+---------------+---------+---------+---------------------------+--------+---------------------------------+
+3793 rows in set (25.96 sec)
+*/
+
+SELECT
 	ps_partkey,
-	sum(ps_supplycost * ps_availqty) as value
-from
+	SUM(ps_supplycost * ps_availqty) AS value
+FROM
 	partsupp,
 	supplier,
 	nation
-where
+WHERE
 	ps_suppkey = s_suppkey
-	and s_nationkey = n_nationkey
-	and n_name = 'GERMANY'
-group by
-	ps_partkey having
-		sum(ps_supplycost * ps_availqty) > (
-			select
-				sum(ps_supplycost * ps_availqty) * 0.0001000000
-			from
-				partsupp,
-				supplier,
-				nation
-			where
-				ps_suppkey = s_suppkey
-				and s_nationkey = n_nationkey
-				and n_name = 'GERMANY'
+	AND s_nationkey = n_nationkey
+	AND n_name = 'GERMANY'
+GROUP BY
+	ps_partkey HAVING
+		SUM(ps_supplycost * ps_availqty) > (
+			SELECT
+				total
+			FROM
+				mv_11
+			WHERE
+				n_name = 'GERMANY'
 		)
-order by
-	value desc
-);
-*/
+ORDER BY
+	value DESC;
+
 EXPLAIN(
 	SELECT
 		ps_partkey,
@@ -55,27 +62,3 @@ EXPLAIN(
 	ORDER BY
 		value DESC
 );
-
-SELECT
-	ps_partkey,
-	SUM(ps_supplycost * ps_availqty) AS value
-FROM
-	partsupp,
-	supplier,
-	nation
-WHERE
-	ps_suppkey = s_suppkey
-	AND s_nationkey = n_nationkey
-	AND n_name = 'GERMANY'
-GROUP BY
-	ps_partkey HAVING
-		SUM(ps_supplycost * ps_availqty) > (
-			SELECT
-				total
-			FROM
-				mv_11
-			WHERE
-				n_name = 'GERMANY'
-		)
-ORDER BY
-	value DESC
