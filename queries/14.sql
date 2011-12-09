a4d62f1559723d82f4f6f315af85f36e501c8776
daiyuda <daiyuda@gmail.com>
2011-12-08 20:05:31 -0500

    Update queries/14.sql

--- queries/14.sql
+++ queries/14.sql
@@ -1,20 +1,25 @@
 -- QUERY 14
 /*
-explain(
-select
-	100.00 * sum(case
-		when p_type like 'PROMO%'
-			then l_extendedprice * (1 - l_discount)
-		else 0
-	end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue
-from
-	lineitem,
-	part
-where
-	l_partkey = p_partkey
-	and l_shipdate >= date '1995-09-01'
-	and l_shipdate < date '1995-09-01' + interval '1' month)
++----+-------------+----------+--------+---------------+---------+---------+-------------------------+---------+-------------+
+| id | select_type | table    | type   | possible_keys | key     | key_len | ref                     | rows    | Extra       |
++----+-------------+----------+--------+---------------+---------+---------+-------------------------+---------+-------------+
+|  1 | SIMPLE      | lineitem | ALL    | NULL          | NULL    | NULL    | NULL                    | 1500000 | Using where |
+|  1 | SIMPLE      | part     | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.lineitem.L_PARTKEY |       1 |             |
++----+-------------+----------+--------+---------------+---------+---------+-------------------------+---------+-------------+
+1 row in set (4.09 sec)
 */
+
+SELECT
+	100.00 * SUM(CASE
+		WHEN p_type LIKE 'PROMO%'
+			THEN price
+		ELSE 0
+	END) / SUM(price) AS promo_revenue
+FROM
+	mv_14
+WHERE
+	l_shipdate >= date '1995-09-01'
+	AND l_shipdate < date '1995-09-01' + interval '1' month;
 
 EXPLAIN(
 SELECT
@@ -29,15 +34,3 @@ WHERE
 	l_shipdate >= date '1995-09-01'
 	AND l_shipdate < date '1995-09-01' + interval '1' month
 );
-
-SELECT
-	100.00 * SUM(CASE
-		WHEN p_type LIKE 'PROMO%'
-			THEN price
-		ELSE 0
-	END) / SUM(price) AS promo_revenue
-FROM
-	mv_14
-WHERE
-	l_shipdate >= date '1995-09-01'
-	AND l_shipdate < date '1995-09-01' + interval '1' month;
