30955205d1f9dcc5844fe17de95a41a3cbae2162
daiyuda <daiyuda@gmail.com>
2011-12-08 20:39:20 -0500

    Update queries/2.sql

--- queries/2.sql
+++ queries/2.sql
@@ -1,4 +1,21 @@
 -- QUERY 2
+/*
++----+--------------------+----------+--------+---------------+---------+---------+---------------------------+--------+----------------------------------------------+
+| id | select_type        | table    | type   | possible_keys | key     | key_len | ref                       | rows   | Extra                                        |
++----+--------------------+----------+--------+---------------+---------+---------+---------------------------+--------+----------------------------------------------+
+|  1 | PRIMARY            | part     | ALL    | PRIMARY       | NULL    | NULL    | NULL                      | 200000 | Using where; Using temporary; Using filesort |
+|  1 | PRIMARY            | partsupp | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.part.P_PARTKEY       |      1 | Using where                                  |
+|  1 | PRIMARY            | supplier | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.partsupp.PS_SUPPKEY  |      1 |                                              |
+|  1 | PRIMARY            | nation   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.supplier.S_NATIONKEY |      1 |                                              |
+|  1 | PRIMARY            | region   | ALL    | PRIMARY       | NULL    | NULL    | NULL                      |      5 | Using where; Using join buffer               |
+|  2 | DEPENDENT SUBQUERY | partsupp | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.part.P_PARTKEY       |      1 |                                              |
+|  2 | DEPENDENT SUBQUERY | supplier | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.partsupp.PS_SUPPKEY  |      1 |                                              |
+|  2 | DEPENDENT SUBQUERY | nation   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.supplier.S_NATIONKEY |      1 |                                              |
+|  2 | DEPENDENT SUBQUERY | region   | eq_ref | PRIMARY       | PRIMARY | 4       | tpch.nation.N_REGIONKEY   |      1 | Using where                                  |
++----+--------------------+----------+--------+---------------+---------+---------+---------------------------+--------+----------------------------------------------+
+162 rows in set (1.09 sec)
+*/
+
 EXPLAIN(
 select
 	s_acctbal,
