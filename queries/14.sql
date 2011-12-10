9aabb413f6d7fd5e2abf4cd40cab1497c71d5bfd
daiyuda <daiyuda@gmail.com>
2011-12-09 20:24:03 -0500

    Update queries/14.sql

--- queries/14.sql
+++ queries/14.sql
@@ -21,16 +21,11 @@ WHERE
 	l_shipdate >= date '1995-09-01'
 	AND l_shipdate < date '1995-09-01' + interval '1' month;
 
-EXPLAIN(
-SELECT
-	100.00 * SUM(CASE
-		WHEN p_type LIKE 'PROMO%'
-			THEN price
-		ELSE 0
-	END) / SUM(price) AS promo_revenue
-FROM
-	mv_14
-WHERE
-	l_shipdate >= date '1995-09-01'
-	AND l_shipdate < date '1995-09-01' + interval '1' month
-);
+/*
++----+-------------+-------+------+---------------+------+---------+------+---------+-------------+
+| id | select_type | table | type | possible_keys | key  | key_len | ref  | rows    | Extra       |
++----+-------------+-------+------+---------------+------+---------+------+---------+-------------+
+|  1 | SIMPLE      | mv_14 | ALL  | NULL          | NULL | NULL    | NULL | 1500000 | Using where |
++----+-------------+-------+------+---------------+------+---------+------+---------+-------------+
+1 row in set (1.79 sec)
+*/
