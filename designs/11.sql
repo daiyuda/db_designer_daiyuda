f8e0ff4c8e807b98c20e4a786eb45a84799078e9
daiyuda <daiyuda@gmail.com>
2011-12-09 20:13:48 -0500

    Update designs/11.sql

--- designs/11.sql
+++ designs/11.sql
@@ -20,14 +20,3 @@ CREATE TABLE mv_11
 
 DROP INDEX name ON mv_11;
 CREATE INDEX name ON mv_11( n_name );
-
-/*
-DROP INDEX suppkey ON partsupp;
-CREATE INDEX suppkey ON partsupp(ps_suppkey);
-
-DROP INDEX nationkey ON supplier;
-CREATE INDEX nationkey ON supplier(s_nationkey);
-
-DROP INDEX name ON nation;
-CREATE INDEX name ON nation( n_name );
-*/
