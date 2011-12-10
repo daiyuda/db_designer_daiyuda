e48083a9d3446310428ec399cf394b711434cf86
daiyuda <daiyuda@gmail.com>
2011-12-09 20:33:36 -0500

    Update queries/15.sql

--- queries/15.sql
+++ queries/15.sql
@@ -49,26 +49,17 @@ WHERE
 ORDER BY
 	s_suppkey;
 
-EXPLAIN(
-	SELECT
-		s_suppkey,
-		s_name,
-		s_address,
-		s_phone,
-		total_revenue
-	FROM
-		supplier,
-		revenue0
-	WHERE
-		s_suppkey = supplier_no
-		AND total_revenue = (
-			SELECT
-				MAX(total_revenue)
-			FROM
-				revenue0
-		)
-	ORDER BY
-		s_suppkey
-);
-
 DROP VIEW revenue0;
+
+/*
++----+-------------+------------+--------+---------------+---------+---------+----------------------+---------+----------------------------------------------+
+| id | select_type | table      | type   | possible_keys | key     | key_len | ref                  | rows    | Extra                                        |
++----+-------------+------------+--------+---------------+---------+---------+----------------------+---------+----------------------------------------------+
+|  1 | PRIMARY     | <derived3> | ALL    | NULL          | NULL    | NULL    | NULL                 |    9968 | Using where; Using temporary; Using filesort |
+|  1 | PRIMARY     | supplier   | eq_ref | PRIMARY       | PRIMARY | 4       | revenue0.supplier_no |       1 |                                              |
+|  3 | DERIVED     | mv_15      | ALL    | NULL          | NULL    | NULL    | NULL                 | 1454720 | Using where; Using temporary; Using filesort |
+|  2 | SUBQUERY    | <derived4> | ALL    | NULL          | NULL    | NULL    | NULL                 |    9968 |                                              |
+|  4 | DERIVED     | mv_15      | ALL    | NULL          | NULL    | NULL    | NULL                 | 1454720 | Using where; Using temporary; Using filesort |
++----+-------------+------------+--------+---------------+---------+---------+----------------------+---------+----------------------------------------------+
+1 row in set (2.29 sec)
+*/
