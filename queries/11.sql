6e7e558a37d1bcfa588aa5dc2e174a66e6763dca
daiyuda <daiyuda@gmail.com>
2011-12-09 19:52:43 -0500

    Update queries/11.sql

--- queries/11.sql
+++ queries/11.sql
@@ -37,28 +37,14 @@ GROUP BY
 ORDER BY
 	value DESC;
 
-EXPLAIN(
-	SELECT
-		ps_partkey,
-		SUM(ps_supplycost * ps_availqty) AS value
-	FROM
-		partsupp,
-		supplier,
-		nation
-	WHERE
-		ps_suppkey = s_suppkey
-		AND s_nationkey = n_nationkey
-		AND n_name = 'GERMANY'
-	GROUP BY
-		ps_partkey HAVING
-			SUM(ps_supplycost * ps_availqty) > (
-				SELECT
-					total
-				FROM
-					mv_11
-				WHERE
-					n_name = 'GERMANY'
-			)
-	ORDER BY
-		value DESC
-);
+/*
++----+-------------+----------+------+-------------------+-----------+---------+-------------------------+------+----------------------------------------------+
+| id | select_type | table    | type | possible_keys     | key       | key_len | ref                     | rows | Extra                                        |
++----+-------------+----------+------+-------------------+-----------+---------+-------------------------+------+----------------------------------------------+
+|  1 | PRIMARY     | nation   | ref  | PRIMARY,name      | name      | 25      | const                   |    1 | Using where; Using temporary; Using filesort |
+|  1 | PRIMARY     | supplier | ref  | PRIMARY,nationkey | nationkey | 4       | tpch.nation.N_NATIONKEY |  400 |                                              |
+|  1 | PRIMARY     | partsupp | ref  | suppkey           | suppkey   | 4       | tpch.supplier.S_SUPPKEY |   20 |                                              |
+|  2 | SUBQUERY    | mv_11    | ref  | name              | name      | 25      | const                   |    1 | Using where                                  |
++----+-------------+----------+------+-------------------+-----------+---------+-------------------------+------+----------------------------------------------+
+3793 rows in set (0.31 sec)
+*/
