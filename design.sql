5087b534a91d2147f9928cd9803befadccccac82
daiyuda <daiyuda@gmail.com>
2011-12-09 20:14:09 -0500

    Update design.sql

--- design.sql
+++ design.sql
@@ -287,7 +287,8 @@ CREATE INDEX orderdate ON mv_10 (o_orderdate);
 DROP TABLE IF EXISTS mv_11;
 CREATE TABLE mv_11
 	SELECT
-		SUM(ps_supplycost * ps_availqty) * 0.0001000000 AS total,
+		ps_partkey,
+		SUM(ps_supplycost * ps_availqty) AS total,
 		n_name
 	FROM
 		partsupp,
@@ -297,19 +298,11 @@ CREATE TABLE mv_11
 		ps_suppkey = s_suppkey
 		AND s_nationkey = n_nationkey
 	GROUP BY
+		ps_partkey,
 		n_name;
 
 DROP INDEX name ON mv_11;
 CREATE INDEX name ON mv_11( n_name );
-
-DROP INDEX suppkey ON partsupp;
-CREATE INDEX suppkey ON partsupp(ps_suppkey);
-
-DROP INDEX nationkey ON supplier;
-CREATE INDEX nationkey ON supplier(s_nationkey);
-
-DROP INDEX name ON nation;
-CREATE INDEX name ON nation( n_name );
 
 -- QUERY 12
 
