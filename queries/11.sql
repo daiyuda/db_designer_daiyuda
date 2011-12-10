1d2afae155ad7d3e2fa4fd87fbedd9639920cdf3
daiyuda <daiyuda@gmail.com>
2011-12-09 20:09:13 -0500

    Update queries/11.sql

--- queries/11.sql
+++ queries/11.sql
@@ -33,14 +33,24 @@ GROUP BY
 ORDER BY
 	value DESC;
 
-/*
-+----+-------------+----------+------+-------------------+-----------+---------+-------------------------+------+----------------------------------------------+
-| id | select_type | table    | type | possible_keys     | key       | key_len | ref                     | rows | Extra                                        |
-+----+-------------+----------+------+-------------------+-----------+---------+-------------------------+------+----------------------------------------------+
-|  1 | PRIMARY     | nation   | ref  | PRIMARY,name      | name      | 25      | const                   |    1 | Using where; Using temporary; Using filesort |
-|  1 | PRIMARY     | supplier | ref  | PRIMARY,nationkey | nationkey | 4       | tpch.nation.N_NATIONKEY |  400 |                                              |
-|  1 | PRIMARY     | partsupp | ref  | suppkey           | suppkey   | 4       | tpch.supplier.S_SUPPKEY |   20 |                                              |
-|  2 | SUBQUERY    | mv_11    | ref  | name              | name      | 25      | const                   |    1 | Using where                                  |
-+----+-------------+----------+------+-------------------+-----------+---------+-------------------------+------+----------------------------------------------+
-3793 rows in set (0.31 sec)
-*/
+explain(
+	SELECT
+	ps_partkey,
+	SUM(total) AS value
+FROM
+	mv_11
+WHERE
+	n_name = 'GERMANY'
+GROUP BY
+	ps_partkey HAVING
+		SUM(total) > (
+			SELECT
+				SUM(total) * 0.0001000000
+			FROM
+				mv_11
+			WHERE
+				n_name = 'GERMANY'
+		)
+ORDER BY
+	value DESC
+);
