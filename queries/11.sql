614822cf00f58893edb0b5f57df3d9814391fbc5
daiyuda <daiyuda@gmail.com>
2011-12-09 20:12:51 -0500

    Update queries/11.sql

--- queries/11.sql
+++ queries/11.sql
@@ -33,24 +33,12 @@ GROUP BY
 ORDER BY
 	value DESC;
 
-explain(
-	SELECT
-	ps_partkey,
-	SUM(total) AS value
-FROM
-	mv_11
-WHERE
-	n_name = 'GERMANY'
-GROUP BY
-	ps_partkey HAVING
-		SUM(total) > (
-			SELECT
-				SUM(total) * 0.0001000000
-			FROM
-				mv_11
-			WHERE
-				n_name = 'GERMANY'
-		)
-ORDER BY
-	value DESC
-);
+/*
++----+-------------+-------+------+---------------+------+---------+-------+------+----------------------------------------------+
+| id | select_type | table | type | possible_keys | key  | key_len | ref   | rows | Extra                                        |
++----+-------------+-------+------+---------------+------+---------+-------+------+----------------------------------------------+
+|  1 | PRIMARY     | mv_11 | ref  | name          | name | 25      | const | 7743 | Using where; Using temporary; Using filesort |
+|  2 | SUBQUERY    | mv_11 | ref  | name          | name | 25      | const | 7743 | Using where                                  |
++----+-------------+-------+------+---------------+------+---------+-------+------+----------------------------------------------+
+3793 rows in set (0.32 sec)
+*/
