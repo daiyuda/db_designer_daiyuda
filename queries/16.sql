9928c1c09245d18c288874a0f26570cc2d863853
daiyuda <daiyuda@gmail.com>
2011-12-09 20:37:10 -0500

    Update queries/16.sql

--- queries/16.sql
+++ queries/16.sql
@@ -39,34 +39,12 @@ ORDER BY
 	p_type,
 	p_size;
 
-EXPLAIN(
-	SELECT
-		p_brand,
-		p_type,
-		p_size,
-		SUM(distinct supplier_cnt) AS supplier_cnt
-	FROM
-		mv_16
-	WHERE
-		p_brand <> 'Brand#45'
-		AND p_type not like 'MEDIUM POLISHED%'
-		AND p_size in (49, 14, 23, 45, 19, 3, 36, 9)
-		AND ps_suppkey not in (
-			SELECT
-				s_suppkey
-			FROM
-				supplier
-			WHERE
-				s_comment like '%Customer%Complaints%'
-		)
-	GROUP BY
-		p_brand,
-		p_type,
-		p_size
-	ORDER BY
-		supplier_cnt DESC,
-		p_brand,
-		p_type,
-		p_size
-);
-
+/*
++----+--------------------+----------+-----------------+---------------+---------+---------+------+--------+----------------------------------------------+
+| id | select_type        | table    | type            | possible_keys | key     | key_len | ref  | rows   | Extra                                        |
++----+--------------------+----------+-----------------+---------------+---------+---------+------+--------+----------------------------------------------+
+|  1 | PRIMARY            | mv_16    | ALL             | NULL          | NULL    | NULL    | NULL | 199993 | Using where; Using temporary; Using filesort |
+|  2 | DEPENDENT SUBQUERY | supplier | unique_subquery | PRIMARY       | PRIMARY | 4       | func |      1 | Using where                                  |
++----+--------------------+----------+-----------------+---------------+---------+---------+------+--------+----------------------------------------------+
+18312 rows in set (2.00 sec)
+*/
